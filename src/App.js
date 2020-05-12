b7e9ff8114876fdd1a323f518fd0297636632339
neorusse <neorusse@gmail.com>
2020-05-12 04:27:23 +0100

    Create landing page message

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					A CI/CD Pipeline created with Jenkins, and used deploy a React
+					Application to AWS S3.
+				</p>
+				<a
+					className="App-link"
+					href="https://www.udacity.com/course/cloud-dev-ops-nanodegree--nd9991"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn Cloud DevOps
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
